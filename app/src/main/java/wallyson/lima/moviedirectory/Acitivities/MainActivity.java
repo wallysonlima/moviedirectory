7169823892919fc774d7fdd932a34e1a192fdc07
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-23 22:32:05 -0200

    Retrieve JSONObject in the MainActivity

--- app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
+++ app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
@@ -4,15 +4,39 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wallyson.lima.moviedirectory.Data.MovieRecyclerViewAdapter;
+import wallyson.lima.moviedirectory.Model.Movie;
 import wallyson.lima.moviedirectory.R;
+import wallyson.lima.moviedirectory.Util.Constants;
+import wallyson.lima.moviedirectory.Util.Prefs;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private MovieRecyclerViewAdapter movieRecyclerViewAdapter;
+    private List<Movie> movieList;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +45,66 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        queue = Volley.newRequestQueue(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movieList = new ArrayList<>();
+
+        Prefs prefs = new Prefs(MainActivity.this);
+        String search = prefs.getSearch();
+        getMovies(search);
+    }
+
+    // Get movies
+    public List<Movie> getMovies(String searchTerm) {
+        movieList.clear();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Constants.URL_LEFT + searchTerm + Constants.URL_RIGHT, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray movieArray = response.getJSONArray("Search");
+
+                    for(int i = 0; i < movieArray.length(); i++) {
+                        JSONObject movieObj = movieArray.getJSONObject(i);
+
+                        Movie movie = new Movie();
+
+                        movie.setTitle(movieObj.getString("Title"));
+                        movie.setYear(movieObj.getString("Year"));
+                        movie.setMovieType(movieObj.getString("Type"));
+                        movie.setPoster(movieObj.getString("Poster"));
+                        movie.setImdbId(movieObj.getString("imdbID"));
+
+                        Log.d("Movies: ", movie.getTitle());
+                    }
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+
+        return movieList;
     }
 
     @Override
---
+++ app/src/main/java/wallyson/lima/moviedirectory/Util/Constants.java
@@ -0,0 +1,6 @@
+package wallyson.lima.moviedirectory.Util;
+
+public class Constants {
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL_RIGHT = "&page=2";
+}
