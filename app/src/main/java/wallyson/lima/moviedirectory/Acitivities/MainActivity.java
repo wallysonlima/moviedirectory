14d8459a7c5d42002d18c1c0118fe93019e8b17c
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-26 15:02:58 -0200

    Fixed the annoyng error that don't show RecyclerViewAdapter

--- app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
+++ app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
@@ -106,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Log.d("Error: ", error.getMessage());
             }
         });
 
