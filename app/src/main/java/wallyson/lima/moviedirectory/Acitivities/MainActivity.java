250661bb4decb669095d14be1beafe1fa4bd3853
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-26 12:49:57 -0200

    Create the MovieDetailsActivity

--- app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
+++ app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
@@ -87,8 +87,8 @@ public class MainActivity extends AppCompatActivity {
                         Movie movie = new Movie();
 
                         movie.setTitle(movieObj.getString("Title"));
-                        movie.setYear(movieObj.getString("Year"));
-                        movie.setMovieType(movieObj.getString("Type"));
+                        movie.setYear("Year Released: " + movieObj.getString("Year"));
+                        movie.setMovieType("Type: " + movieObj.getString("Type"));
                         movie.setPoster(movieObj.getString("Poster"));
                         movie.setImdbId(movieObj.getString("imdbID"));
 
---
+++ app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MovieDetailActivity.java
@@ -0,0 +1,15 @@
+package wallyson.lima.moviedirectory.Acitivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wallyson.lima.moviedirectory.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
