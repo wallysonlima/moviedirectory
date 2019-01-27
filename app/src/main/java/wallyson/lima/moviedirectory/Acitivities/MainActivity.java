55d1069e9770062e155fc91dadec93fd66404e7d
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-27 18:48:16 -0200

    Finish the application

--- app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
+++ app/src/main/java/wallyson/lima/moviedirectory/Acitivities/MainActivity.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -53,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                showInputDialog();
             }
         });
 
@@ -61,16 +63,72 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        movieList = new ArrayList<>();
-
         Prefs prefs = new Prefs(MainActivity.this);
         String search = prefs.getSearch();
+
+        movieList = new ArrayList<>();
+
+
         // getMovies(search);
         movieList = getMovies(search);
 
         movieRecyclerViewAdapter = new MovieRecyclerViewAdapter(this, movieList);
         recyclerView.setAdapter(movieRecyclerViewAdapter);
         movieRecyclerViewAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.new_search) {
+            showInputDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showInputDialog() {
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_view, null);
+        final EditText newSearchEdt = view.findViewById(R.id.searchEdt);
+        Button submitButton = view.findViewById(R.id.submitButton);
+
+        alertDialogBuilder.setView(view);
+        dialog = alertDialogBuilder.create();
+        dialog.show();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Prefs prefs = new Prefs(MainActivity.this);
+
+                if (!newSearchEdt.getText().toString().isEmpty()) {
+                    String search = newSearchEdt.getText().toString();
+                    prefs.setSearch(search);
+                    movieList.clear();
+
+                    getMovies(search);
+
+                    //movieRecyclerViewAdapter.notifyDataSetChanged();
+                }
+
+                dialog.dismiss();
+                
+            }
+        });
     }
 
     // Get movies
@@ -116,33 +174,5 @@ public class MainActivity extends AppCompatActivity {
         queue.add(jsonObjectRequest);
 
         return movieList;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.new_search) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void showInputDialog() {
-        alertDialogBuilder = new AlertDialog.Builder(this);
-        View view = getLayoutInflater().inflate(R.layout.dialog_view, null);
-
     }
 }
